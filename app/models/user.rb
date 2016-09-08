3ccc8b6408ab9def021d62951d128a641cb48b8e
BenRoss92 <benjaminjackross@gmail.com>
2016-09-08 16:29:21 +0100

    confirms password is correct to create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ property :id, Serial
 property :email, String
 property :password, BCryptHash
 
+attr_reader :password
+attr_accessor :password_confirmation
+
+validates_confirmation_of :password
+
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -4,11 +4,21 @@ feature 'let a user sign up' do
   scenario 'a user signs up on the /sign_up page' do
     visit '/sign_up'
     fill_in('email', with: 'joseph@coffeenutcase.com')
-    fill_in('password', with: 'x')
+    fill_in('password', with: 'iamjoseph')
+    fill_in('password_confirmation', with: 'iamjoseph')
     click_button('Sign Up')
     expect(page).to have_content('Welcome joseph@coffeenutcase.com')
     expect(User.first.email).to eq('joseph@coffeenutcase.com')
     expect(User.count).to eq(1)
+  end
+
+  scenario 'password confirmation' do
+    visit '/sign_up'
+    fill_in('email', with: 'joseph@coffeenutcase.com')
+    fill_in('password', with: 'iamjoseph')
+    fill_in('password_confirmation', with: 'iamben')
+    click_button('Sign Up')
+    expect(User.count).to eq(0)
   end
 
 end
