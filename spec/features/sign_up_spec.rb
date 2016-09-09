3f2424b370433fef0d758bab17bec554e9cb7f61
James Stonehill <jamesstonehill@gmail.com>
2016-09-09 11:00:40 +0100

    validates for correct and unique email input

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -23,4 +23,14 @@ feature 'let a user sign up' do
     expect(page).to have_content("Passwords do not match")
   end
 
+  scenario 'user can not sign up with a blank email address' do
+    visit '/sign_up'
+    fill_in('email', with: '')
+    fill_in('password', with: 'asdf')
+    fill_in('password_confirmation', with: 'asdf')
+    click_button('Sign Up')
+
+    expect(User.count).to eq(0)
+    expect(page).to have_current_path("/sign_up")
+  end
 end
